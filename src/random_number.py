1f456f9b5b052f68cdcea2f08ff800f5756d82d3
LeottaAlberto <leottaalberto43@gmail.com>
2025-11-27 17:54:38 +0100

    feat(random_number): add generate_random_number and implement logic

--- src/random_number.py
+++ src/random_number.py
@@ -4,3 +4,15 @@
 #   Output:
 #   The random number is: 4
 #
+import random
+
+MAX_RAND = 100
+
+
+def generate_random_number(max: int) -> int:
+    return random.random() * max
+
+
+if __name__ == "__main__":
+    rand_num = int(generate_random_number(MAX_RAND))
+    print("The random number is: ", rand_num)
